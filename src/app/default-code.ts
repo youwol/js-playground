3bcb5f93b61f927f9b4fc58570a9203df6c181aa
youwol-guillaume <greinisch@youwol.com>
2023-05-02 17:30:01 +0200

    ♻️ default-code => improve

--- src/app/default-code.ts
+++ src/app/default-code.ts
@@ -1,23 +1,56 @@
-export const example = `
-return async ({cdnClient}) => {
-    await cdnClient.install({
-    // bootstrap will come with its dependencies installed first (e.g. popper.js)
-        modules:['bootstrap#^4.0.0'],
-    // CSS are explicitly imported 
-        css: ['bootstrap#^4.4.0~bootstrap.min.css']
+export const example = `return async ({cdnClient}) => {
+    // Reactive version of https://getbootstrap.com/docs/4.0/components/dropdowns/
+    // using flux-view (see https://l.youwol.com/doc/@youwol/flux-view)
+    
+    const {FV, rx} = await cdnClient.install({
+        modules:['bootstrap#^4.4.0', '@youwol/flux-view#^1.1.0'],
+        css: ['bootstrap#^4.4.0~bootstrap.min.css'],
+        aliases: {
+            FV: '@youwol/flux-view',
+            // rxjs is an indirect dependency of @youwol/flux-view
+            rx: 'rxjs'
+        }
     })
-    const div = document.createElement('div')
-    div.innerHTML =  \`
-<div class="dropdown">
-    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        Dropdown button
-    </button>
-  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-    <a class="dropdown-item">Action</a>
-    <a class="dropdown-item">Another action</a>
-    <a class="dropdown-item">Something else here</a>
-  </div>
-</div>\`
-    return div
+    const source$ = new rx.BehaviorSubject([{date:new Date()}])
+    const selected$ = new rx.Subject()
+    const vDOM = {
+        children:[
+            {
+                class:'dropdown',
+                children: [{ 
+                        tag:'button', class:'btn btn-secondary dropdown-toggle', type:'button',
+                        customAttributes:{'data-toggle':'dropdown'},
+                        // innerText is updated each time selected$ emit a new item
+                        innerText: FV.attr$(
+                            selected$, 
+                            (item) => item.date.toLocaleString(), 
+                            {untilFirst:'Dropdown button'}
+                        )
+                    },
+                    {
+                        class:'dropdown-menu',
+                        // new children are added each time source$ emit
+                        children: FV.childrenAppendOnly$(
+                            source$,
+                            (item) => ({
+                                class:'dropdown-item fv-pointer',
+                                innerText: item.date.toLocaleString(), 
+                                onclick: () => selected$.next(item)
+                            })
+                        )
+                    }
+                ]
+            },
+            { class:'my-2' },
+            {
+                tag:'button', class:'btn btn-secondary',
+                innerText: 'Add item',
+                onclick: () => source$.next([{date:new Date()}])
+            }
+        ]
+    }
+    // FV.render return an HTMLElement, the one displayed when code is ran.
+    return FV.render(vDOM)
 }
+
 `
