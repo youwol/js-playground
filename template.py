f0bb3ad79770b7763dcd68dd9ba9b16528de8a26
jehadmelad <jd.melad@gmail.com>
2023-11-22 19:23:31 +0100

    ⬆️  template.py => `@youwol/flux-view`: "^1.0.3" => "^1.2.0"

--- template.py
+++ template.py
@@ -19,7 +19,7 @@ pkg_json = parse_json(folder_path / "package.json")
 
 externals = {
     "@youwol/cdn-client": "^2.0.6",
-    "@youwol/flux-view": "^1.0.3",
+    "@youwol/flux-view": "^1.2.0",
     "@youwol/fv-code-mirror-editors": "^0.3.1",
     "rxjs": "^6.5.5",
     "@youwol/http-clients": "^2.0.5",
