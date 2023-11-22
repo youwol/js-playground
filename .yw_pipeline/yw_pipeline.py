c5ac3dac50541de86d798b71989dbc706d1b9dc9
jehadmelad <jd.melad@gmail.com>
2023-11-22 19:01:55 +0100

    📝 yw_pipeline.py => rename the display name: `@youwol/js-playground` => `JS playground`

--- .yw_pipeline/yw_pipeline.py
+++ .yw_pipeline/yw_pipeline.py
@@ -17,7 +17,7 @@ class PipelineFactory(IPipelineFactory):
     async def get(self, _env: YouwolEnvironment, context: Context):
         config = PipelineConfig(
             target=BrowserApp(
-                displayName="@youwol/js-playground",
+                displayName="JS playground",
                 execution=Execution(standalone=True),
                 graphics=BrowserAppGraphics(
                     appIcon={"class": "far fa-laugh-beam fa-2x"}, fileIcon={}
